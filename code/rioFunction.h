27c2fece3c8cf4c1266e8857729825a4d08797aa
Antoine Poirier-Rouillard <poia2409@usherbrooke.ca>
2019-02-18 10:16:19 +0100

    Added comments
    to : rioFunction.c rioFunction.h

--- code/rioFunction.h
+++ code/rioFunction.h
@@ -1,3 +1,15 @@
+/**
+ * \file rioFunction.h
+ * \brief Processing functions
+ * \author Louis Schuck
+ * \version 0.2
+ * \date 18/02/2019
+ *
+ * Module that contains multiple processing functions
+ *
+ */
+
+//Header file containing the prototypes
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -17,72 +29,74 @@
 	#define DT_DIR 4
 #endif
 
-/* @function
- *		transform a value which vary between vmin and vmax, to an other one that vary between min and max
+/* \fn nmap(float value, float vmin, float vmax, float min, float max);
+ * \brief function that transforms a value which vary between vmin and vmax, to an other one that vary between min and max
+ * 
+ * \param
+ * 	value :	the reference value that we want to transform
+ * 	vmin :	the minimum the value can get
+ *	vmax :	the maximum the value can get
+ *	min :	the new minimum we want to set the value to
+ *	max :	the new maximum we want to set the value to
  *
- * @param
- *		value : 	the reference value that we want to transform
- *		vmin :		the minimum the value can get
- *		vmax :		the maximum the value can get
- *		min :		the new minimum we want to set the value to
- *		max :		the new maximum we want to set the value to
- *
- * @return
+ * \return
  *		return a float number between min and max
  */
 float nmap(float value, float vmin, float vmax, float min, float max);
 
-/* @function
- *		copy an existing list of float
+/* \fn cpyList(float* list, int size);
+ * \brief function that copies an existing list of float
  *
- * @param
+ * \param
  *		list : 	the original list we want to copy
  *		size :	the size of the list
  *
- * @return
+ * \return
  *		return a list of float
  */
 float* cpyList(float* list, int size);
 
-/* @function
- *		free a 2D matrice from the memory
+/* \fn destructMatrice2D(void** matrice, int size);
+ * \brief function that frees a 2D matrice from the memory
  *
- * @param
+ * \param
  *		matrice : 	the matrice we want to free from the memory
  *		size :	the size of the first dimmension of the list
  *
- * @return
+ * \return
  *		return a list of float
  */
 void destructMatrice2D(void** matrice, int size);
 
-/* @function
- *		shuffle the values in a given list
+/* \fn shuffleList(float* list, int size);
+ * \brief function that shuffles the values in a given list
  *
- * @param
+ * \param
  *		list : 	the list we want to shuffle
  *		size :	the size of the list
+ * \return
+ * 		void
  */
 void shuffleList(float* list, int size);
 
-/* @function
- *		get the number of files in a given directory
+/* \fn getNumberOfFilesInDirectory(char* path);
+ * \brief function that gets the number of files in a given directory
  *
- * @param
+ * \param
  *		path : 	the path of the directory
  *
- * @return
+ * \return
  		the number of files in the given directory
  */
 int getNumberOfFilesInDirectory(char* path);
 
-/* @function
- *		get the name of each files in the given directory
+/* \fn getAllFileNameInDirectory(char* path);
+ * \brief function that gets the name of each files in the given directory
  *
- * @param
+ * \param
  *		path : 	the path of the directory
  *
- * @return
+ * \return
  		a list of string containing the names of each files
  */
 char** getAllFileNameInDirectory(char* path);
