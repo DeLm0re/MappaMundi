38374933b23614c212bc45142a0927e40ac3dba9
Unknown <theo.hipault@gmail.com>
2019-05-10 15:48:16 +0200

    Added files for stats

---
+++ code/stats.c
@@ -0,0 +1,10 @@
+/**
+ * \file stats.c
+ * \brief functions to gather statistics on the NN and GN performances, and to parse this data into files
+ * \author Théo Hipault
+ * \version 0.1
+ * \date 10/05/2019
+ *
+ * Module that contain all the functions used to generate statistics on the neural networks
+ *
+ */
---
+++ code/stats.h
@@ -0,0 +1,15 @@
+/**
+ * \file stats.h
+ * \brief functions to gather statistics on the NN and GN performances, and to parse this data into files
+ * \author Théo Hipault
+ * \version 0.1
+ * \date 10/05/2019
+ *
+ * Module that contain all the prototypes of functions in stats.c
+ *
+ */
+
+#ifndef H_STATS
+    #define H_STATS
+
+#endif
